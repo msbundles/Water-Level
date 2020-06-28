b3b7f4decfc510d70cb8c661261c31001327a9d9
msbundles <25093511+msbundles@users.noreply.github.com>
2020-06-28 00:31:06 -0500

    The Python version is still a mockup. I am not a Python pro and this will be a little bit more of a challenge than the web version

--- Python/index.py
+++ Python/index.py
@@ -1,6 +1,6 @@
 import requests
-fRom = input("what is the first day and time you would like to get data from? (YYYYMMDD HR:MN) ")
-to = input("what is the first day and time you would like to get data from? (YYYYMMDD HR:MN) ")
+fRom = input("What is the day and time you would like to get data from? (YYYYMMDD HR:MN) ")
+to = input("What is the day and time you would like to get data to? (YYYYMMDD HR:MN) ")
 url = f"https://tidesandcurrents.noaa.gov/api/datagetter?begin_date={fRom}&end_date={to}&station=9087044&product=water_level&datum=igld&units=metric&time_zone=gmt&application=web_services&format=csv"
 data = requests.get(url)
 name = (f"NOAA_lake_height_data_{fRom}_{to}.csv")
